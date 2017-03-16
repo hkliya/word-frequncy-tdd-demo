3985987fddf30a9d0d87bedcd7fb19a75797c601
Seaborn Lee <seabornlee.cn@gmail.com>
2017-03-16 20:36:54 +0800

    Revert "Did it"
    This reverts commit 8d26da13649680477e4d216678745c078b02c13b.

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -8,46 +8,12 @@ chai.use(sinonChai);
 
 var main = require("../lib/main.js");
 
-describe("Word frequency", function(){
-    it("return empty given empty string", function(){
-        var result = main('');
+describe("测试描述", function(){
+    it("测试用例1", function(){
+
+        var result = main();
         var expect_string = '';
         
-        expect(expect_string).to.equal(result);
-    });
-
-    it("return word frequency given one word", function () {
-        var result = main('the');
-        var expect_string = 'the 1';
-
-        expect(expect_string).to.equal(result);
-    });
-
-    it("return word frequency given two different words", function () {
-        var result = main('the is');
-        var expect_string = 'the 1\r\nis 1';
-
-        expect(expect_string).to.equal(result);
-    });
-
-    it("return word frequency given duplicated words", function () {
-        var result = main('the is the');
-        var expect_string = 'the 2\r\nis 1';
-
-        expect(expect_string).to.equal(result);
-    });
-
-    it("return word frequency given duplicated words need sort", function () {
-        var result = main('the is is');
-        var expect_string = 'is 2\r\nthe 1';
-
-        expect(expect_string).to.equal(result);
-    });
-
-    it("return word frequency given words split with multiple spaces", function () {
-        var result = main('the  is');
-        var expect_string = 'the 1\r\nis 1';
-
         expect(expect_string).to.equal(result);
     });
 });
