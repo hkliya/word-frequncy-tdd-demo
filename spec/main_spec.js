11f7fd8815d818e2a43bf9e4d9ed30285add74f6
Seaborn Lee <seabornlee.cn@gmail.com>
2017-03-17 13:19:13 +0800

    Did it

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -8,12 +8,35 @@ chai.use(sinonChai);
 
 var main = require("../lib/main.js");
 
-describe("测试描述", function(){
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
+describe("Word Frequency", function(){
+    it("returns empty string given empty string", function(){
+        var result = main('');
+        expect(result).to.equal('');
     });
+
+    it("returns string given one word", function(){
+        var result = main('he');
+        expect(result).to.equal('he 1');
+    });
+
+    it("returns string given two different words", function(){
+        var result = main('he is');
+        expect(result).to.equal('he 1\r\nis 1');
+    });
+
+    it("returns string given duplicated words", function(){
+        var result = main('he is he');
+        expect(result).to.equal('he 2\r\nis 1');
+    });
+
+    it("returns string given duplicated words need to be sorted", function(){
+        var result = main('he is is');
+        expect(result).to.equal('is 2\r\nhe 1');
+    });
+
+    it("returns string given words splited by multiple spaces", function(){
+        var result = main('he    is');
+        expect(result).to.equal('he 1\r\nis 1');
+    });
+
 });
